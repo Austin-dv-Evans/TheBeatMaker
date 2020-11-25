6386f447444df6c97fa0e8491d289fe84fea9430
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-11-25 11:56:47 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+function playKick(){
+    const myKick = document.getElementById("kick")
+    myKick.play();
+}
+function playClap(){
+    const myClap = document.getElementById("clap")
+    myClap.play();
+}
+function playSnare(){
+    const mySnare = document.getElementById("snare")
+    mySnare.play();
+}
+function playHat(){
+    const myHat = document.getElementById("hat")
+    myHat.play();
+}
+function playCrash(){
+    const myCrash = document.getElementById("crash")
+    myCrash.play();
+}
+function playPerc(){
+    const myPerc = document.getElementById("perc")
+    myPerc.play();
+}
+function playKick2(){
+    const myKick2 = document.getElementById("kick2")
+    myKick2.play();
+}
+function playSnare2(){
+    const mySnare2 = document.getElementById("snare2")
+    mySnare2.play();
+}
+
